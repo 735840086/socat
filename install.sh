6cdbb69592b34ee4259142473c1c35b018dab61c
hhroot <105219296+735840086@users.noreply.github.com>
2025-04-11 14:22:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="4.4.5"
-DOWNLOAD_HOST="https://raw.githubusercontent.com/735840086/hhminer/main/hhminer"
+DOWNLOAD_HOST="https://raw.githubusercontent.com/735840086/hhminer/main"
 ORIGIN_EXEC="socat-${VERSION}"
 
 SERVICE_NAME="socatservice"
